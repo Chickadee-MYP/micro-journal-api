fd48cc59bf183bc28c28a1e134fa633291145c9c
Preeti Das <preetidas_a@yahoo.com>
2022-04-14 11:26:07 -0500

    seed data

--- bin/seed.js
+++ bin/seed.js
@@ -60,43 +60,168 @@ export const authors = [
 
 export const posts = [
   {
-    authorId: 4,
-    content: 'The journey of a thousand miles begins with one step',
+    authorId: 3,
+    content: 'The journey of a thousand miles begins with one step - Lao Tzu',
   },
   {
     authorId: 3,
-    content: 'The importance of small steps',
+    content: 'Bring presence into whatever you do - Eckhart Tolle',
+  },
+  {
+    authorId: 6,
+    content:
+      'My wife told me to stop impersonating a flamingo. I had to put my foot down.',
+  },
+  {
+    authorId: 6,
+    content: "I failed math so many times at school, I can't even count.",
+  },
+  {
+    authorId: 6,
+    content: ' I used to have a handle on life, but then it broke',
   },
   {
     authorId: 2,
-    content: 'Bring presence into whatever you do',
+    content: "Don't think... Become - Blinkous Galadrigal",
+  },
+  {
+    authorId: 6,
+    content:
+      'To the well-organized mind, death is but the next great adventure.- Albus Dumbledore',
   },
   {
     authorId: 1,
-    content: 'Doing is never enough if you neglect Being',
+    content: 'When in doubt, go to the library. - Ron Weasley',
+  },
+  {
+    authorId: 4,
+    content: 'It takes a lot of balls to golf the way I do.',
+  },
+  {
+    authorId: 5,
+    content:
+      'Fear of a name only increases fear of the thing itself. — Hermione Granger',
+  },
+  {
+    authorId: 1,
+    content:
+      'I could be a morning person — but only if morning started at noon!',
+  },
+  {
+    authorId: 3,
+    content:
+      'What I need is perspective. The illusion of depth, created by a frame, the arrangement of shapes on a flat surface.',
+  },
+  {
+    authorId: 1,
+    content:
+      'I heard there were a bunch of break-ins over at the car park. That is wrong on so many levels.',
+  },
+  {
+    authorId: 3,
+    content:
+      "We lived, as usual, by ignoring. Ignoring isn't the same as ignorance, you have to work at it.",
+  },
+  {
+    authorId: 1,
+    content: 'The early bird catches the worm, but what about the early worm?',
+  },
+  {
+    authorId: 5,
+    content:
+      'It does not do well to dwell on dreams and forget to live. - Albus Dumbledore',
+  },
+  {
+    authorId: 5,
+    content:
+      'We delight in the beauty of the butterfly, but rarely admit the changes it has gone through to achieve that beauty. - Maya Angelou',
+  },
+  {
+    authorId: 1,
+    content:
+      "Don't you hate it when someone answers their own questions? I do.",
+  },
+  {
+    authorId: 6,
+    content:
+      'I want to die peacefully in my sleep, like my grandfather… Not screaming and yelling like the passengers in his car.',
+  },
+  {
+    authorId: 5,
+    content:
+      'Three Principles for Effective Workplace Communication - 1. Professionalism 2. Clarity 3. Humility.',
+  },
+  {
+    authorId: 1,
+    content:
+      'The future, the present, and the past walked into a bar. Things got a little tense.',
+  },
+  {
+    authorId: 3,
+    content: 'I know they say that money talks, but all mine says is Goodbye.',
+  },
+  {
+    authorId: 2,
+    content:
+      "Water is the most essential element in life, because without it you can't make coffee.",
+  },
+  {
+    authorId: 1,
+    content:
+      'If the pen is mightier than the sword, a sharpie must be plain deadly!',
   },
 ];
 
 export const comments = [
   {
-    content: 'great content',
+    content: 'LOL',
     authorId: 5,
-    postId: 3,
+    postId: 9,
   },
   {
     content: 'beautiful quote',
     authorId: 2,
-    postId: 4,
-  },
-  {
-    content: 'mind-blowing',
-    authorId: 3,
     postId: 1,
   },
   {
+    content: 'mind-blowing',
+    authorId: 6,
+    postId: 17,
+  },
+  {
     content: 'cannot agree more',
-    authorId: 3,
+    authorId: 5,
     postId: 2,
+  },
+  {
+    content: 'hilarious',
+    authorId: 3,
+    postId: 3,
+  },
+  {
+    content: 'Margaret Atwood is a genius',
+    authorId: 3,
+    postId: 12,
+  },
+  {
+    content: 'The early worm dies U+1F604',
+    authorId: 1,
+    postId: 15,
+  },
+  {
+    content: "I can't live without coffee",
+    authorId: 2,
+    postId: 23,
+  },
+  {
+    content: 'Me too U+1F644',
+    authorId: 3,
+    postId: 23,
+  },
+  {
+    content: 'High-Five Potter-head',
+    authorId: 2,
+    postId: 8,
   },
 ];
 
