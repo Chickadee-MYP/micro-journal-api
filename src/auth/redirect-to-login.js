be923a814ae9a8f0c705d906efba4a7641041ac0
Mark Warner <mark.warner@verizon.com>
2022-04-15 10:56:38 -0500

    Update postman collection

--- src/auth/redirect-to-login.js
+++
@@ -1,8 +0,0 @@
-const redirectToLogin = (req, res, next) => {
-  const currentUser = req.user;
-  if (!currentUser) {
-    res.redirect('/auth/login');
-  } else next();
-};
-
-export default redirectToLogin;
