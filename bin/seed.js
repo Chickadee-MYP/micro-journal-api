5ce573940b244eb5a999d4db1434f01c0908f238
Mark Warner <mark.warner@verizon.com>
2022-04-14 22:46:50 -0500

    Rename author model to user

--- bin/seed.js
+++ bin/seed.js
@@ -1,13 +1,13 @@
 import logger from '../src/config/logger.js';
 import associateModels from '../src/config/associate-models.js';
-import Author from '../src/api/author/author.model.js';
+import User from '../src/api/user/user.model.js';
 import Post from '../src/api/post/post.model.js';
 import Comment from '../src/api/comment/comment.model.js';
 import Address from '../src/api/address/address.model.js';
 import Like from '../src/api/like/like.model.js';
 import db from '../src/config/db.js';
 
-export const authors = [
+export const users = [
   {
     username: 'mark',
     password: '$2b$10$Dn46iKnUQAshp9PwfacxyuilNmEc7VkxrAp/0cHBGerh1PfRBiUNK',
@@ -60,113 +60,113 @@ export const authors = [
 
 export const posts = [
   {
-    authorId: 3,
+    userId: 3,
     content: 'The journey of a thousand miles begins with one step - Lao Tzu',
   },
   {
-    authorId: 3,
+    userId: 3,
     content: 'Bring presence into whatever you do - Eckhart Tolle',
   },
   {
-    authorId: 6,
+    userId: 6,
     content:
       'My wife told me to stop impersonating a flamingo. I had to put my foot down.',
   },
   {
-    authorId: 6,
+    userId: 6,
     content: "I failed math so many times at school, I can't even count.",
   },
   {
-    authorId: 6,
+    userId: 6,
     content: ' I used to have a handle on life, but then it broke',
   },
   {
-    authorId: 2,
+    userId: 2,
     content: "Don't think... Become - Blinkous Galadrigal",
   },
   {
-    authorId: 6,
+    userId: 6,
     content:
       'To the well-organized mind, death is but the next great adventure.- Albus Dumbledore',
   },
   {
-    authorId: 1,
+    userId: 1,
     content: 'When in doubt, go to the library. - Ron Weasley',
   },
   {
-    authorId: 4,
+    userId: 4,
     content: 'It takes a lot of balls to golf the way I do.',
   },
   {
-    authorId: 5,
+    userId: 5,
     content:
       'Fear of a name only increases fear of the thing itself. — Hermione Granger',
   },
   {
-    authorId: 1,
+    userId: 1,
     content:
       'I could be a morning person — but only if morning started at noon!',
   },
   {
-    authorId: 3,
+    userId: 3,
     content:
       'What I need is perspective. The illusion of depth, created by a frame, the arrangement of shapes on a flat surface.',
   },
   {
-    authorId: 1,
+    userId: 1,
     content:
       'I heard there were a bunch of break-ins over at the car park. That is wrong on so many levels.',
   },
   {
-    authorId: 3,
+    userId: 3,
     content:
       "We lived, as usual, by ignoring. Ignoring isn't the same as ignorance, you have to work at it.",
   },
   {
-    authorId: 1,
+    userId: 1,
     content: 'The early bird catches the worm, but what about the early worm?',
   },
   {
-    authorId: 5,
+    userId: 5,
     content:
       'It does not do well to dwell on dreams and forget to live. - Albus Dumbledore',
   },
   {
-    authorId: 5,
+    userId: 5,
     content:
       'We delight in the beauty of the butterfly, but rarely admit the changes it has gone through to achieve that beauty. - Maya Angelou',
   },
   {
-    authorId: 1,
+    userId: 1,
     content:
       "Don't you hate it when someone answers their own questions? I do.",
   },
   {
-    authorId: 6,
+    userId: 6,
     content:
       'I want to die peacefully in my sleep, like my grandfather… Not screaming and yelling like the passengers in his car.',
   },
   {
-    authorId: 5,
+    userId: 5,
     content:
       'Three Principles for Effective Workplace Communication - 1. Professionalism 2. Clarity 3. Humility.',
   },
   {
-    authorId: 1,
+    userId: 1,
     content:
       'The future, the present, and the past walked into a bar. Things got a little tense.',
   },
   {
-    authorId: 3,
+    userId: 3,
     content: 'I know they say that money talks, but all mine says is Goodbye.',
   },
   {
-    authorId: 2,
+    userId: 2,
     content:
       "Water is the most essential element in life, because without it you can't make coffee.",
   },
   {
-    authorId: 1,
+    userId: 1,
     content:
       'If the pen is mightier than the sword, a sharpie must be plain deadly!',
   },
@@ -175,52 +175,52 @@ export const posts = [
 export const comments = [
   {
     content: 'LOL',
-    authorId: 5,
+    userId: 5,
     postId: 9,
   },
   {
     content: 'beautiful quote',
-    authorId: 2,
+    userId: 2,
     postId: 1,
   },
   {
     content: 'mind-blowing',
-    authorId: 6,
+    userId: 6,
     postId: 17,
   },
   {
     content: 'cannot agree more',
-    authorId: 5,
+    userId: 5,
     postId: 2,
   },
   {
     content: 'hilarious',
-    authorId: 3,
+    userId: 3,
     postId: 3,
   },
   {
     content: 'Margaret Atwood is a genius',
-    authorId: 3,
+    userId: 3,
     postId: 12,
   },
   {
     content: 'The early worm dies U+1F604',
-    authorId: 1,
+    userId: 1,
     postId: 15,
   },
   {
     content: "I can't live without coffee",
-    authorId: 2,
+    userId: 2,
     postId: 23,
   },
   {
     content: 'Me too U+1F644',
-    authorId: 3,
+    userId: 3,
     postId: 23,
   },
   {
     content: 'High-Five Potter-head',
-    authorId: 2,
+    userId: 2,
     postId: 8,
   },
 ];
@@ -229,42 +229,42 @@ export const likes = [
   {
     likableId: 20,
     likableType: 'post',
-    authorId: 5,
+    userId: 5,
   },
   {
     likableId: 10,
     likableType: 'comment',
-    authorId: 2,
+    userId: 2,
   },
   {
     likableId: 1,
     likableType: 'post',
-    authorId: 2,
+    userId: 2,
   },
   {
     likableId: 6,
     likableType: 'comment',
-    authorId: 1,
+    userId: 1,
   },
   {
     likableId: 6,
     likableType: 'comment',
-    authorId: 5,
+    userId: 5,
   },
   {
     likableId: 7,
     likableType: 'comment',
-    authorId: 3,
+    userId: 3,
   },
   {
     likableId: 1,
     likableType: 'post',
-    authorId: 4,
+    userId: 4,
   },
   {
     likableId: 8,
     likableType: 'post',
-    authorId: 4,
+    userId: 4,
   },
 ];
 
@@ -338,7 +338,7 @@ const seed = async force => {
     await associateModels();
     await db.sync({ force });
     await Address.bulkCreate(addresses, { validate: true });
-    await Author.bulkCreate(authors, { validate: true });
+    await User.bulkCreate(users, { validate: true });
     await Post.bulkCreate(posts, { validate: true });
     await Comment.bulkCreate(comments, { validate: true });
     await Like.bulkCreate(likes, { validate: true });
